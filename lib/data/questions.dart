f1e0a79b6c91d9b81b540ac4828111b6cad21efb
MohamedBilalKR <mohamedbilal6637@gmail.com>
2024-05-27 11:49:45 +0530

    Quiz App

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Which Nation Won The Football World Cup in 2022?',
+    [
+      'Argentina',
+      'Portugal',
+      'France',
+      'Brazil',
+    ],
+  ),
+  QuizQuestion(
+    'Which Player Scored Most Goals in Football History?',
+    [
+      'Cristiano Ronaldo',
+      'Lionel Messi',
+      'Pele',
+      'Diego Maradona',
+    ],
+  ),
+  QuizQuestion(
+    'Which Player Provided Most Assists in Football History?',
+    [
+      'Lionel Messi',
+      'Cristiano Ronaldo',
+      'Ferenc Puskas',
+      'Johann Cruyff',
+    ],
+  ),
+  QuizQuestion(
+    'Which Football Club Has Won The Most No.of Champions League Titles?',
+    [
+      'Real Madrid',
+      'FC Barcelona',
+      'AC Milan',
+      'FC Bayern Munich',
+    ],
+  ),
+  QuizQuestion(
+    "How Many Ballon D'or Did Lionel Messi Won?",
+    [
+      '7',
+      '5',
+      '2',
+      '8',
+    ],
+  ),
+  QuizQuestion(
+    'How Many Champions League Titles Did Cristiano Ronaldo Won?',
+    [
+      '5',
+      '4',
+      '2',
+      '6',
+    ],
+  ),
+];
